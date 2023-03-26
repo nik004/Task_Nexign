9771a54a6f1043d5b9f6a1122af2e99f7860bdda
Nikolai K <kuznik@mail.ru>
2023-03-26 10:13:44 +0300

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import data.base.DataBase;
 public class Main {
 
     public static void main(String[] args) {
-        String cdrFileName = "./src/main/cdr.txt";
+        String cdrFileName = "./src/cdr.txt";
         String reportCatalog = "./src/reports/";
 
         if (args.length!=0) cdrFileName = args[0];
